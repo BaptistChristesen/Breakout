f5588ae4b60201c4d0bbf649d6c2ad2f7ba5a144
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-03-08 09:31:44 -0600

    Initial Commit

---
+++ Breakout/BreakoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakoutApp.swift
+//  Breakout
+//
+//  Created by caden christesen on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BreakoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
